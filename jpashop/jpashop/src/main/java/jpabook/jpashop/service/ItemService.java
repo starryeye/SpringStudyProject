9a974b6c07077c7469a8dc5d7b0985a88657be20
starryeye <see2818@naver.com>
2022-03-26 19:59:26 +0900

    [starryeye, 2022.03.26] 제목  - 상품 수정 (변경 감지 > merge) 내용  - 상품 수정 (변경 감지 > merge) 원인  - 개선  -

--- jpashop/jpashop/src/main/java/jpabook/jpashop/service/ItemService.java
+++ jpashop/jpashop/src/main/java/jpabook/jpashop/service/ItemService.java
@@ -1,5 +1,6 @@
 package jpabook.jpashop.service;
 
+import jpabook.jpashop.domain.item.Book;
 import jpabook.jpashop.domain.item.Item;
 import jpabook.jpashop.repository.ItemRepository;
 import lombok.RequiredArgsConstructor;
@@ -18,6 +19,17 @@ public class ItemService {
     @Transactional
     public void saveItem(Item item) {
         itemRepository.save(item);
+    }
+
+    @Transactional
+    public void updateItem(Long itemId, int price, String name, int stockQuantity) {
+        Item findItem = itemRepository.findOne(itemId);
+//        findItem.setPrice(param.getPrice());
+//        findItem.setName(param.getName());
+//        findItem.setStockQuantity(param.getStockQuantity());
+        findItem.setPrice(price);
+        findItem.setName(name);
+        findItem.setStockQuantity(stockQuantity);
     }
 
     public List<Item> findItems() {
