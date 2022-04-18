8fd6d2b119d59c3b953c975ca0b7fb936200f00d
starryeye <see2818@naver.com>
2022-04-18 23:49:12 +0900

    [starryeye, 2022.04.18] - 스프링 mvc 실습 핵심비즈니스로직 내용  - 상품 도메인 개발 원인  - 개선  -

---
+++ item-service/item-service/src/test/java/hello/itemservice/domain/item/ItemRepositoryTest.java
@@ -0,0 +1,61 @@
+package hello.itemservice.domain.item;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class ItemRepositoryTest {
+
+    ItemRepository itemRepository = new ItemRepository();
+
+    @AfterEach
+    void afterEach() {
+        itemRepository.clearStore();
+    }
+
+    @Test
+    void save() {
+        Item item = new Item("itemA", 10000, 10);
+
+        Item savedItem = itemRepository.save(item);
+
+        Item findItem = itemRepository.findById(item.getId());
+        assertThat(findItem).isEqualTo(savedItem);
+    }
+
+    @Test
+    void findAll() {
+
+        Item item1 = new Item("itemA", 10000, 10);
+        Item item2 = new Item("itemB", 20000, 20);
+
+        itemRepository.save(item1);
+        itemRepository.save(item2);
+
+        List<Item> result = itemRepository.findAll();
+
+        assertThat(result.size()).isEqualTo(2);
+    }
+
+    @Test
+    void updateItem() {
+
+        Item item = new Item("item1", 10000, 10);
+
+        Item savedItem = itemRepository.save(item);
+        Long itemId = savedItem.getId();
+
+        Item updateParam = new Item("item2", 20000, 20);
+        itemRepository.update(itemId, updateParam);
+
+        Item findItem = itemRepository.findById(itemId);
+
+        assertThat(findItem.getItemName()).isEqualTo(updateParam.getItemName());
+        assertThat(findItem.getPrice()).isEqualTo(updateParam.getPrice());
+        assertThat(findItem.getQuantity()).isEqualTo(updateParam.getQuantity());
+    }
+}
