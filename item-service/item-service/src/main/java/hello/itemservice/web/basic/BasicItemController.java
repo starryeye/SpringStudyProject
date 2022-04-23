96e3dd36bf353a23110657f43ffe39ed4b8a1a4a
starryeye <see2818@naver.com>
2022-04-23 17:14:42 +0900

    [starryeye, 2022.04.23] - spring mvc 실전 redirectAttribute 내용  - spring mvc 실전 redirectAttribute 원인  - 개선  -

--- item-service/item-service/src/main/java/hello/itemservice/web/basic/BasicItemController.java
+++ item-service/item-service/src/main/java/hello/itemservice/web/basic/BasicItemController.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import javax.annotation.PostConstruct;
 import java.util.List;
@@ -25,7 +26,7 @@ public class BasicItemController {
         return "basic/items";
     }
 
-    @GetMapping("/{itemId}")
+    @GetMapping("/{itemId}") //    /basic/items/{itemId} 가 적용됨. (쿼리파라미터 포함)
     public String item(@PathVariable long itemId, Model model) {
         Item item = itemRepository.findById(itemId);
         model.addAttribute("item", item);
@@ -76,10 +77,18 @@ public class BasicItemController {
         return "basic/item";
     }
 
-    @PostMapping("/add")
+    //@PostMapping("/add")
     public String addItemV5(Item item) {
         itemRepository.save(item);
         return "redirect:/basic/items/" + item.getId();
+    }
+
+    @PostMapping("/add")
+    public String addItemV6(Item item, RedirectAttributes redirectAttributes) {
+        Item itemSaved = itemRepository.save(item);
+        redirectAttributes.addAttribute("itemId", itemSaved.getId());
+        redirectAttributes.addAttribute("status", true);
+        return "redirect:/basic/items/{itemId}";
     }
 
     @GetMapping("/{itemId}/edit")
