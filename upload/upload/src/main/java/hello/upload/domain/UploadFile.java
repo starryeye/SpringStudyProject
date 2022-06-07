26323450375bffd99b96b8f10001a996fa393471
starryeye <see2818@naver.com>
2022-06-07 23:30:58 +0900

    [starryeye, 2022.06.07] - 스프링 파일업로드 다운로드 비즈니스 로직 추가 내용  - 스프링 파일업로드 다운로드 비즈니스 로직 추가 개선  -

--- upload/upload/src/main/java/hello/upload/domain/UploadFile.java
+++ upload/upload/src/main/java/hello/upload/domain/UploadFile.java
@@ -7,4 +7,9 @@ public class UploadFile {
 
     private String uploadFileName;
     private String storeFileName;
+
+    public UploadFile(String uploadFileName, String storeFileName) {
+        this.uploadFileName = uploadFileName;
+        this.storeFileName = storeFileName;
+    }
 }
