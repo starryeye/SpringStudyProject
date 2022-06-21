6e86ae45798ecb4e0d49cc05c886dd92d846eea3
starryeye <see2818@naver.com>
2022-06-22 01:46:56 +0900

    [starryeye, 2022.06.22] - 트랜잭션 적용해보기 내용  -  트랜잭션 사용 개선  -

---
+++ jdbc/jdbc/src/main/java/hello/jdbc/service/MemberServiceV2.java
@@ -0,0 +1,69 @@
+package hello.jdbc.service;
+
+import hello.jdbc.domain.Member;
+import hello.jdbc.repository.MemberRepositoryV2;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.SQLException;
+
+@Slf4j
+@RequiredArgsConstructor
+public class MemberServiceV2 {
+
+    private final DataSource dataSource;
+    private final MemberRepositoryV2 memberRepository;
+
+    public void accountTransfer(String fromId, String toId, int money) throws SQLException {
+
+        Connection con = dataSource.getConnection();
+        try {
+            //트랜잭션 시작
+            con.setAutoCommit(false);
+
+            //비즈니스 로직
+            bizLogic(con, fromId, toId, money);
+
+            //성공시 커밋
+           con.commit();
+        } catch (Exception e) {
+
+            //실패시 롤백
+            con.rollback();
+            throw new IllegalStateException(e);
+        } finally {
+            release(con);
+        }
+
+
+    }
+
+    private void bizLogic(Connection con, String fromId, String toId, int money) throws SQLException {
+        Member fromMember = memberRepository.findById(con, fromId);
+        Member toMember = memberRepository.findById(con, toId);
+
+        memberRepository.update(con, fromId, fromMember.getMoney() - money);
+        validation(toMember);
+        memberRepository.update(con, toId, toMember.getMoney() + money);
+    }
+
+    private void release(Connection con) {
+        if(con != null) {
+            try {
+                con.close();
+                con.setAutoCommit(true);
+            } catch (Exception e) {
+                log.info("error", e);
+            }
+        }
+    }
+
+    //예외가 발생하는 것을 알아보기 위한 ...
+    private void validation(Member toMember) {
+        if(toMember.getMemberId().equals("ex")) {
+            throw new IllegalStateException("이체중 예외 발생");
+        }
+    }
+}
