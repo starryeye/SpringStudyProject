b6bb93455d54517c5ce594742b4c908f48faeadc
starryeye <see2818@naver.com>
2022-07-06 01:16:07 +0900

    [starryeye, 2022.07.06] - 스프링이 제공하는 데이터 접근 예외 적용 내용  -  SQLExceptionTranslator 변환기, DataAccessException 데이터접근 예외최상위(스프링 제공 예외 추상화) 개선  -

--- jdbc/jdbc/src/test/java/hello/jdbc/service/MemberServiceV4Test.java
+++ jdbc/jdbc/src/test/java/hello/jdbc/service/MemberServiceV4Test.java
@@ -4,6 +4,7 @@ import hello.jdbc.domain.Member;
 import hello.jdbc.repository.MemberRepository;
 import hello.jdbc.repository.MemberRepositoryV3;
 import hello.jdbc.repository.MemberRepositoryV4_1;
+import hello.jdbc.repository.MemberRepositoryV4_2;
 import lombok.extern.slf4j.Slf4j;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.AfterEach;
