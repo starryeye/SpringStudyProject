e935d2b2b785623bb40b5d25888cecc8c140e604
starryeye <see2818@naver.com>
2022-08-25 01:22:56 +0900

    [starryeye, 2022.08.24] - JPA, JPA는 예외처리를 위해 예외 변환 AOP 프록시를 만든다. 내용  - JPA는 예외처리를 위해 예외 변환 AOP 프록시를 만든다. 개선  -

--- itemservice-db/itemservice-db/src/test/java/hello/itemservice/domain/ItemRepositoryTest.java
+++ itemservice-db/itemservice-db/src/test/java/hello/itemservice/domain/ItemRepositoryTest.java
@@ -4,6 +4,7 @@ import hello.itemservice.repository.ItemRepository;
 import hello.itemservice.repository.ItemSearchCond;
 import hello.itemservice.repository.ItemUpdateDto;
 import hello.itemservice.repository.memory.MemoryItemRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -18,6 +19,7 @@ import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+@Slf4j
 @Transactional
 @SpringBootTest
 class ItemRepositoryTest {
