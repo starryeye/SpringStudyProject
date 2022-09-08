975ca0ffd47ae5d94bb094045d0000896ba35a6f
starryeye <see2818@naver.com>
2022-09-09 03:29:06 +0900

    [starryeye, 2022.09.09] - 외부 트랜잭션, 내부 트랜잭션 모두 커밋일 경우의 Test 코드 내용  - TEST 코드를 통한 학습 개선  -

--- springtx/springtx/src/test/java/hello/springtx/propagation/BasicTxTest.java
+++ springtx/springtx/src/test/java/hello/springtx/propagation/BasicTxTest.java
@@ -78,4 +78,21 @@ public class BasicTxTest {
         txManager.rollback(tx2);
         log.info("트랜잭션1 롤백 완료");
     }
+
+    @Test
+    void inner_commit() {
+        log.info("외부 트랜잭션 시작");
+        TransactionStatus outer = txManager.getTransaction(new DefaultTransactionAttribute());
+        log.info("outer.isNewTransaction()={}", outer.isNewTransaction()); //true
+
+        log.info("내부 트랜잭션 시작");
+        TransactionStatus inner = txManager.getTransaction(new DefaultTransactionAttribute());
+        log.info("inner.isNewTransaction()={}", inner.isNewTransaction()); //false
+
+        log.info("내부 트랜잭션 커밋");
+        txManager.commit(inner);
+
+        log.info("외부 트랜잭션 커밋");
+        txManager.commit(outer);
+    }
 }
