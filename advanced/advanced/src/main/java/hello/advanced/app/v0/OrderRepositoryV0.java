af7c4bbf481e2cb61f883bea4f300f4b05ce9eab
starryeye <see2818@naver.com>
2022-09-15 20:52:36 +0900

    [starryeye, 2022.09.15] - 기본예제 생성(Controller, Service, Repository) 내용  - 기본예제 생성(Controller, Service, Repository) 개선  -

---
+++ advanced/advanced/src/main/java/hello/advanced/app/v0/OrderRepositoryV0.java
@@ -0,0 +1,25 @@
+package hello.advanced.app.v0;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+@Repository
+@RequiredArgsConstructor
+public class OrderRepositoryV0 {
+
+    public void save(String itemId) {
+        //저장 로직
+        if(itemId.equals("ex")) { //id에 ex가 포함되면 예외 발생
+            throw new IllegalStateException("예외 발생!");
+        }
+        sleep(1000); //상품을 주문하는데는 1초정도 걸린다고 가정
+    }
+
+    private void sleep(int millis) {
+        try {
+            Thread.sleep(millis);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
