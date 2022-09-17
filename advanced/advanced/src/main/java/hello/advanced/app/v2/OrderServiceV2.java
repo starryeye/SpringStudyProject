04fbafc731d60c029f72164817c668a85a88b037
starryeye <see2818@naver.com>
2022-09-18 01:03:38 +0900

    [starryeye, 2022.09.18] - 로그추적기 개발 V2 적용 내용  - 기본적인 로그추적기 V2 C/S/R 에 적용 개선  -

---
+++ advanced/advanced/src/main/java/hello/advanced/app/v2/OrderServiceV2.java
@@ -0,0 +1,36 @@
+package hello.advanced.app.v2;
+
+import hello.advanced.trace.TraceId;
+import hello.advanced.trace.TraceStatus;
+import hello.advanced.trace.hellotrace.HelloTraceV1;
+import hello.advanced.trace.hellotrace.HelloTraceV2;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class OrderServiceV2 {
+
+    private final OrderRepositoryV2 orderRepository;
+    private final HelloTraceV2 trace;
+
+    public void orderItem(TraceId traceId, String itemId) {
+
+        TraceStatus status = null;
+
+        try{
+            //로그추적기-시작
+            status = trace.beginSync(traceId,"OrderServiceV1.orderItem()");
+
+            //비즈니스
+            orderRepository.save(status.getTraceId(), itemId);
+
+            //로그추적기-끝
+            trace.end(status);
+        } catch (Exception e) {
+            trace.exception(status, e);
+            throw e;//예외를 꼭 다시 던져주어야 한다.
+            //로그 기능 때문에 흐름을 변경시키면 안된다.
+        }
+    }
+}
