f50518c0a8e126a96897381774a096b0f82be45f
starryeye <see2818@naver.com>
2022-09-20 02:19:59 +0900

    [starryeye, 2022.09.20] - 로그추적기 정식 버전 적용 내용  - 파라미터로 traceId를 넘기는 일은 더이상 없다. 개선  -

---
+++ advanced/advanced/src/main/java/hello/advanced/app/v3/OrderRepositoryV3.java
@@ -0,0 +1,45 @@
+package hello.advanced.app.v3;
+
+import hello.advanced.trace.TraceStatus;
+import hello.advanced.trace.logtrace.LogTrace;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+@Repository
+@RequiredArgsConstructor
+public class OrderRepositoryV3 {
+
+    private final LogTrace trace;
+
+    public void save(String itemId) {
+
+        TraceStatus status = null;
+
+        try{
+            //로그추적기-시작
+            status = trace.begin("OrderRepositoryV1.save()");
+
+            //비즈니스
+            //저장 로직
+            if(itemId.equals("ex")) { //id에 ex가 포함되면 예외 발생
+                throw new IllegalStateException("예외 발생!");
+            }
+            sleep(1000); //상품을 주문하는데는 1초정도 걸린다고 가정
+
+            //로그추적기-끝
+            trace.end(status);
+        } catch (Exception e) {
+            trace.exception(status, e);
+            throw e;//예외를 꼭 다시 던져주어야 한다.
+            //로그 기능 때문에 흐름을 변경시키면 안된다.
+        }
+    }
+
+    private void sleep(int millis) {
+        try {
+            Thread.sleep(millis);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
