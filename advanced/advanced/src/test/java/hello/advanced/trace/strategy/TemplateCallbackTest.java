914028b8023a1c3587f00ba461f5b18152170e6f
starryeye <see2818@naver.com>
2022-09-28 00:00:06 +0900

    [starryeye, 2022.09.28] - Spring의 주요 디자인 패턴 - 템플릿 콜백 패턴 학습 내용  - 템플릿 콜백 패턴 학습 개선  -

--- advanced/advanced/src/test/java/hello/advanced/trace/strategy/TemplateCallbackTest.java
+++ advanced/advanced/src/test/java/hello/advanced/trace/strategy/TemplateCallbackTest.java
@@ -33,7 +33,7 @@ public class TemplateCallbackTest {
      * 템플릿 콜백 패턴 - 람다
      */
     @Test
-    void callbackV1() {
+    void callbackV2() {
         TimeLogTemplate template = new TimeLogTemplate();
         template.execute(() -> log.info("비즈니스 로직1 실행"));
 
