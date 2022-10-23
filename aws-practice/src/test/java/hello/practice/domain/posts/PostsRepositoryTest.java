7030fc2206860ad634c36cff8a0e39c44916a935
starryeye <see2818@naver.com>
2022-10-24 04:55:53 +0900

    [starryeye, 2022.10.24] - AWS-Practice JpaRepository 생성 내용  - JpaRepository 생성 및 TEST코드 작성  - H2 개선

---
+++ aws-practice/src/test/java/hello/practice/domain/posts/PostsRepositoryTest.java
@@ -0,0 +1,48 @@
+package hello.practice.domain.posts;
+
+import org.assertj.core.api.Assertions;
+import org.junit.After;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.List;
+
+import static org.junit.Assert.*;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PostsRepositoryTest {
+
+    @Autowired
+    PostsRepository postsRepository;
+
+    @After
+    public void cleanup() {
+        postsRepository.deleteAll();
+    }
+
+    @Test
+    public void 게시글저장_불러오기() {
+        //given
+        String title = "test title";
+        String content = "test content";
+
+        postsRepository.save(Posts.builder()
+                .title(title)
+                .content(content)
+                .author("see2818@naver.com")
+                .build());
+
+        //when
+        List<Posts> postsList = postsRepository.findAll();
+
+        //then
+        Posts posts = postsList.get(0);
+        Assertions.assertThat(posts.getTitle()).isEqualTo(title);
+        Assertions.assertThat(posts.getContent()).isEqualTo(content);
+    }
+
+}
