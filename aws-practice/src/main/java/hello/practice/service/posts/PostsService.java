4ef0ed4bb82cf0f349b115e5644ed2dc5517f2b4
starryeye <see2818@naver.com>
2022-10-24 22:59:38 +0900

    [starryeye, 2022.10.24] - AWS-Practice 게시판 게시글 수정, 조회 기능 개발 내용  - 게시판 게시글 수정, 조회 기능 개발 및 TEST 코드 작성 개선

--- aws-practice/src/main/java/hello/practice/service/posts/PostsService.java
+++ aws-practice/src/main/java/hello/practice/service/posts/PostsService.java
@@ -1,7 +1,10 @@
 package hello.practice.service.posts;
 
+import hello.practice.domain.posts.Posts;
 import hello.practice.domain.posts.PostsRepository;
+import hello.practice.web.dto.PostsResponseDto;
 import hello.practice.web.dto.PostsSaveRequestDto;
+import hello.practice.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -16,5 +19,23 @@ public class PostsService {
     @Transactional
     public Long save(PostsSaveRequestDto requestDto) {
         return postsRepository.save(requestDto.toEntity()).getId();
+    }
+
+    @Transactional
+    public Long update(Long id, PostsUpdateRequestDto requestDto) {
+
+        Posts posts = postsRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
+
+        posts.update(requestDto.getTitle(), requestDto.getContent());
+
+        return id;
+    }
+
+    public PostsResponseDto findById(Long id) {
+        Posts entity = postsRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
+
+        return new PostsResponseDto(entity);
     }
 }
