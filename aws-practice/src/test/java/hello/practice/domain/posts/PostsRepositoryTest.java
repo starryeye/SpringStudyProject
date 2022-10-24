8841d1a466f07f3a86f84554080d3727bb9203e5
starryeye <see2818@naver.com>
2022-10-24 23:22:26 +0900

    [starryeye, 2022.10.24] - AWS-Practice Jpa Auditing 적용 내용  - 등록/수정 시간 자동화 개선

--- aws-practice/src/test/java/hello/practice/domain/posts/PostsRepositoryTest.java
+++ aws-practice/src/test/java/hello/practice/domain/posts/PostsRepositoryTest.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 import static org.junit.Assert.*;
@@ -45,4 +46,25 @@ public class PostsRepositoryTest {
         Assertions.assertThat(posts.getContent()).isEqualTo(content);
     }
 
+    @Test
+    public void BaseTimeEntity_등록() {
+        //given
+        LocalDateTime now = LocalDateTime.of(2022,10,24,0,0,0);
+        postsRepository.save(Posts.builder()
+                .title("title")
+                .content("content")
+                .author("author")
+                .build());
+
+        //when
+        List<Posts> postsList = postsRepository.findAll();
+
+        //then
+        Posts posts = postsList.get(0);
+
+        System.out.println(">>>>>>> createDate="+ posts.getCreatedDate() + ", modifiedDate=" + posts.getModifiedDate());
+
+        Assertions.assertThat(posts.getCreatedDate()).isAfter(now);
+        Assertions.assertThat(posts.getModifiedDate()).isAfter(now);
+    }
 }
