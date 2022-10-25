0b9d0309f4888381480a1dde6dca821ba181eb93
starryeye <see2818@naver.com>
2022-10-25 16:25:21 +0900

    [starryeye, 2022.10.25] - AWS-Practice 등록/삭제/수정 기능 및 화면 개발 내용  - 게시글 등록/삭제/수정 기능 및 화면 개발 개선

--- aws-practice/src/main/java/hello/practice/service/posts/PostsService.java
+++ aws-practice/src/main/java/hello/practice/service/posts/PostsService.java
@@ -2,13 +2,17 @@ package hello.practice.service.posts;
 
 import hello.practice.domain.posts.Posts;
 import hello.practice.domain.posts.PostsRepository;
+import hello.practice.web.dto.PostsListResponseDto;
 import hello.practice.web.dto.PostsResponseDto;
 import hello.practice.web.dto.PostsSaveRequestDto;
 import hello.practice.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
-import javax.transaction.Transactional;
+import java.util.List;
+import java.util.stream.Collectors;
+
 
 @RequiredArgsConstructor
 @Service
@@ -32,10 +36,26 @@ public class PostsService {
         return id;
     }
 
+    @Transactional
+    public void delete(Long id) {
+        Posts posts = postsRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
+
+        postsRepository.delete(posts);
+    }
+
     public PostsResponseDto findById(Long id) {
         Posts entity = postsRepository.findById(id)
                 .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
 
         return new PostsResponseDto(entity);
     }
+
+    @Transactional(readOnly = true)
+    public List<PostsListResponseDto> findAllDesc() {
+        return postsRepository.findAllDesc().stream()
+                .map(PostsListResponseDto::new)
+                .collect(Collectors.toList());
+    }
+
 }
