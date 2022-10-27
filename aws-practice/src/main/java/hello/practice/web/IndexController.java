61ca42333c8168c0dedc48dcbfc529fb35a27fb8
starryeye <see2818@naver.com>
2022-10-28 00:24:18 +0900

    [starryeye, 2022.10.28] - Session 최적화 내용  - ArgumentResolver 활용 개선

--- aws-practice/src/main/java/hello/practice/web/IndexController.java
+++ aws-practice/src/main/java/hello/practice/web/IndexController.java
@@ -1,5 +1,6 @@
 package hello.practice.web;
 
+import hello.practice.config.auth.LoginUser;
 import hello.practice.config.auth.dto.SessionUser;
 import hello.practice.service.posts.PostsService;
 import hello.practice.web.dto.PostsResponseDto;
@@ -9,20 +10,15 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-import javax.servlet.http.HttpSession;
-
 @RequiredArgsConstructor
 @Controller
 public class IndexController {
 
     private final PostsService postsService;
-    private final HttpSession httpSession;
 
     @GetMapping("/")
-    public String index(Model model) {
+    public String index(Model model, @LoginUser SessionUser user) {
         model.addAttribute("posts", postsService.findAllDesc());
-
-        SessionUser user = (SessionUser) httpSession.getAttribute("user");
 
         if(user != null) {
             model.addAttribute("userName", user.getName());
