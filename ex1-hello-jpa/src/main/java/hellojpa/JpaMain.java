9a0c5c27b46f5c6b3bd316e81963a3ec11f65c85
starryeye <see2818@naver.com>
2022-11-16 00:31:06 +0900

    [starryeye, 2022.11.16] - EntityManagerFactory, EntityManager 내용  - EntityManagerFactory, EntityManager 학습 개선

--- ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
+++ ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
@@ -1,8 +1,34 @@
 package hellojpa;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+
 public class JpaMain {
 
     public static void main(String[] args) {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
 
+        EntityManager entityManager = emf.createEntityManager();
+
+        EntityTransaction tx = entityManager.getTransaction();
+        tx.begin();
+
+        try {
+            Member member = new Member();
+            member.setId(1L);
+            member.setName("HelloA");
+
+            entityManager.persist(member);
+
+            tx.commit();
+        } catch (Exception e) {
+            tx.rollback();
+        } finally {
+            entityManager.close();
+        }
+
+        emf.close();
     }
 }
