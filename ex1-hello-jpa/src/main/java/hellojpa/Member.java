fa88ab522c1bf8ce9ae61e44784698c012a67a39
starryeye <see2818@naver.com>
2022-11-20 04:45:56 +0900

    [starryeye, 2022.11.20] - 기본키 매핑 - Table 전략 내용  - @TableGenerator, @GeneratedValue(strategy = GenerationType.TABLE, generator = "~") 개선

--- ex1-hello-jpa/src/main/java/hellojpa/Member.java
+++ ex1-hello-jpa/src/main/java/hellojpa/Member.java
@@ -4,14 +4,14 @@ import javax.persistence.*;
 
 
 @Entity
-@SequenceGenerator(
+@TableGenerator(
         name = "MEMBER_SEQ_GENERATOR",
-        sequenceName = "MEMBER_SEQ", //매핑할 데이터베이스 시퀀스 이름
-        initialValue = 1, allocationSize = 1)
+        table = "my_sequences",
+        pkColumnName = "member_seq", allocationSize = 1)
 public class Member {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "MEMBER_SEQ_GENERATOR")
+    @GeneratedValue(strategy = GenerationType.TABLE, generator = "MEMBER_SEQ_GENERATOR")
     private Long id;
 
     @Column(name = "name", nullable = false)
