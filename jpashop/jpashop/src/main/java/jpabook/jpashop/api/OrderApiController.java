1e99c49d96c7f96b455b02a35d050cc3f23601fd
starryeye <see2818@naver.com>
2022-12-31 22:43:54 +0900

    [starryeye, 2022.12.31] - Order 조회 api.. 컬랙션 조회, DTO 변환, 패치 조인, 페이징 사용 내용  - Order 조회 api.. 컬랙션 조회, DTO 변환, 패치 조인  - 다대일은 패치조인  - 페이징 사용  - 일대다는 batch size 글로벌 적용 -> 인쿼리  - 1 + N이 1 + 1로 가능해짐. 개선

--- jpashop/jpashop/src/main/java/jpabook/jpashop/api/OrderApiController.java
+++ jpashop/jpashop/src/main/java/jpabook/jpashop/api/OrderApiController.java
@@ -9,6 +9,7 @@ import jpabook.jpashop.repository.OrderSearch;
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.time.LocalDateTime;
@@ -59,6 +60,19 @@ public class OrderApiController {
         return result;
     }
 
+    @GetMapping("/api/v3.1/orders")
+    public List<OrderDto> ordersV3_page(
+            @RequestParam(value = "offset", defaultValue = "0") int offset,
+            @RequestParam(value = "limit", defaultValue = "100") int limit) {
+
+        List<Order> orders = orderRepository.findAllWithMemberDelivery(offset, limit);
+
+        List<OrderDto> result = orders.stream()
+                .map(o -> new OrderDto(o))
+                .collect(Collectors.toList());
+
+        return result;
+    }
     @Data //@Getter
     static class OrderDto {
 
