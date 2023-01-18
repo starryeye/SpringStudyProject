e53602b30255c7d70688f4588c9480fd4b46ad48
starryeye <see2818@naver.com>
2023-01-18 23:50:35 +0900

    [starryeye, 2023.01.18] - Spring Data JPA, 페이징과 정렬(컨트롤러) 내용 - Page, Pageable을 controller의 각각 리턴타입, 파라미터로 적용할 수 있다. 개선

--- data-jpa/data-jpa/src/main/java/study/datajpa/controller/MemberController.java
+++ data-jpa/data-jpa/src/main/java/study/datajpa/controller/MemberController.java
@@ -1,6 +1,10 @@
 package study.datajpa.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -26,9 +30,23 @@ public class MemberController {
         return member.getUsername();
     }
 
+    @GetMapping("/members")
+    public Page<Member> list(Pageable pageable) {
+        Page<Member> page = memberRepository.findAll(pageable);
+        return page;
+    }
+
+    @GetMapping("/members_page")
+    public Page<Member> list2(@PageableDefault(size = 12, sort = "username", direction = Sort.Direction.DESC) Pageable pageable) {
+        Page<Member> page = memberRepository.findAll(pageable);
+        return page;
+    }
+
     //findMember 를 테스트하기 위해 db에 데이터를 넣어줌
     @PostConstruct
     public void init() {
-        memberRepository.save(new Member("userA"));
+        for (int i = 0; i < 100; i++) {
+            memberRepository.save(new Member(i, "user" + i));
+        }
     }
 }
