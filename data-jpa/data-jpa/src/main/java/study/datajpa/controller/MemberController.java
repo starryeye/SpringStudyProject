8aea2d1a4856e0bd281ad25a5dda4f3f35e10402
starryeye <see2818@naver.com>
2023-01-19 00:15:54 +0900

    [starryeye, 2023.01.19] - Spring Data JPA, 페이징과 정렬(컨트롤러) 내용 - DTO로 변환해서 리턴해야 한다 개선

--- data-jpa/data-jpa/src/main/java/study/datajpa/controller/MemberController.java
+++ data-jpa/data-jpa/src/main/java/study/datajpa/controller/MemberController.java
@@ -8,6 +8,7 @@ import org.springframework.data.web.PageableDefault;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+import study.datajpa.dto.MemberDto;
 import study.datajpa.entity.Member;
 import study.datajpa.repository.MemberRepository;
 
@@ -31,9 +32,9 @@ public class MemberController {
     }
 
     @GetMapping("/members")
-    public Page<Member> list(Pageable pageable) {
-        Page<Member> page = memberRepository.findAll(pageable);
-        return page;
+    public Page<MemberDto> list(Pageable pageable) {
+        return memberRepository.findAll(pageable)
+                .map(MemberDto::new);
     }
 
     @GetMapping("/members_page")
