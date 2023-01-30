0a2932b47cf0655b4a0dd16ce3ed264387d599b6
starryeye <see2818@naver.com>
2023-01-30 22:14:32 +0900

    [starryeye, 2023.01.30] - WebSecurityConfigurerAdapter 가 deprecated 되어 SecurityFilterChain 을 빈으로 등록 내용 - WebSecurityConfigurerAdapter 가 deprecated 되어 SecurityFilterChain 을 빈으로 등록 개선

--- jwt-tutorial/src/main/java/study/jwttutorial/config/SecurityConfig.java
+++ jwt-tutorial/src/main/java/study/jwttutorial/config/SecurityConfig.java
@@ -1,14 +1,17 @@
 package study.jwttutorial.config;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
-import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.filter.CorsFilter;
 import study.jwttutorial.jwt.JwtAccessDeniedHandler;
 import study.jwttutorial.jwt.JwtAuthenticationEntryPoint;
 import study.jwttutorial.jwt.JwtSecurityConfig;
@@ -18,27 +21,22 @@ import study.jwttutorial.jwt.TokenProvider;
  * @EnableWebSecurity : 기본적인 웹 보안을 활성화 하겠다는 의미
  * -> 추가적인 설정을 위해서, WebSecurityConfigurer 를 implements 하거나 WebSecurityConfigurerAdapter 를 extends 하는 방법이 있다.
  * -> WebSecurityConfigurerAdapter 가 deprecated 되었다...
+ * -> SecurityFilterChain 을 빈으로 등록해주는 filterChain 메서드 구현
+ * -> 기존에 오버라이딩 해서 구현했던 로직이 filterChain 으로 일부 들어갔다.
  *
- * @EnableGlobalMethodSecurity : @PreAuthorize 어노테이션을 메서드 단위로 사용하기 위해 적용한 어노테이션
+ * @EnableGlobalMethodSecurity (prePostEnabled = true) : @PreAuthorize 어노테이션을 메서드 단위로 사용하기 위해 적용한 어노테이션
+ * -> deprecated 되어 @EnableMethodSecurity 사용
  */
 @EnableWebSecurity
-@EnableGlobalMethodSecurity(prePostEnabled = true)
-public class SecurityConfig extends WebSecurityConfigurerAdapter {
+@EnableMethodSecurity
+@RequiredArgsConstructor
+public class SecurityConfig {
 
+    // jwt 패키지에서 ComponentScan 을 통해 등록한 빈들을 주입 받는다.
     private final TokenProvider tokenProvider;
     private final JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint;
     private final JwtAccessDeniedHandler jwtAccessDeniedHandler;
-
-    // jwt 패키지에 각각 ComponentScan 을 통해 등록한 빈들을 주입 받는다.
-    public SecurityConfig(
-            TokenProvider tokenProvider,
-            JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint,
-            JwtAccessDeniedHandler jwtAccessDeniedHandler
-    ) {
-        this.tokenProvider = tokenProvider;
-        this.jwtAuthenticationEntryPoint = jwtAuthenticationEntryPoint;
-        this.jwtAccessDeniedHandler = jwtAccessDeniedHandler;
-    }
+    private final CorsFilter corsFilter;
 
     /**
      * PasswordEncoder 는 BCryptPasswordEncoder 를 사용
@@ -48,22 +46,13 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         return new BCryptPasswordEncoder();
     }
 
-    //h2-console 하위 모든 요청과 파비콘 관련 요청은 Spring Security 로직을 수행하지 않기 위함.
-    @Override
-    public void configure(WebSecurity web) throws Exception {
-        web
-                .ignoring()
-                .antMatchers(
-                        "/h2-console/**",
-                        "/favicon.ico"
-                );
-    }
-
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity
                 // 토큰 방식을 사용하기 때문에 csrf 설정은 disable 시킨다.
                 .csrf().disable()
+
+                .addFilterBefore(corsFilter, UsernamePasswordAuthenticationFilter.class)
 
                 // Exception 을 핸들링 할때 사용 할 AuthenticationEntryPoint 와 AccessDeniedHandler 를 jwt 패키지에서 만든 클래스로 적용하도록 함
                 .exceptionHandling()
@@ -71,6 +60,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .accessDeniedHandler(jwtAccessDeniedHandler)
 
                 //h2-console 을 위한 설정 추가
+                //enable h2-console
                 .and()
                 .headers()
                 .frameOptions()
@@ -82,15 +72,17 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
 
                 .and()
-                .authorizeRequests() //HttpServletRequest를 사용하는 요청들에 대한 접근 제한을 설정하겠다.
-                .antMatchers("/api/hello").permitAll() // /api/hello 에 대한 요청은 인증없이 접근을 허용하겠다.
-                .antMatchers("/api/authenticate").permitAll() // /api/authenticate (로그인) 에 대한 요청은 인증없이 접근을 허용하겠다.
-                .antMatchers("/api/signup").permitAll() // /api/signup (회원가입) 에 대한 요청은 인증없이 접근을 허용하겠다.
+                .authorizeHttpRequests() //HttpServletRequest를 사용하는 요청들에 대한 접근 제한을 설정하겠다.
+                .antMatchers("/api/hello").permitAll() //다음 URL 에 대한 요청은 인증 없이 접근을 허용하겠다.
+                .antMatchers("/api/authenticate").permitAll()
+                .antMatchers("/api/signup").permitAll()
+                .requestMatchers(PathRequest.toH2Console()).permitAll() //h2-console 관련 요청은 인증 제한 없이 접근 허용
                 .anyRequest().authenticated() //나머지 요청들은 모두 인증을 받겠다.
 
                 // JwtFilter 를 addFilterBefore 로 등록했던 JwtSecurityConfig 클래스 적용
                 .and()
                 .apply(new JwtSecurityConfig(tokenProvider));
 
+        return httpSecurity.build();
     }
 }
