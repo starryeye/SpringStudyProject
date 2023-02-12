0d60199c00659e258d69abe4dc7bb100f7c106eb
starryeye <see2818@naver.com>
2023-02-12 23:36:46 +0900

    [starryeye, 2023.02.12] - Querydsl, 결과 조회 Test 내용  - querydsl의 결과 조회 Test 개선

--- querydsl/src/test/java/study/querydsl/QuerydslBasicTest.java
+++ querydsl/src/test/java/study/querydsl/QuerydslBasicTest.java
@@ -1,5 +1,6 @@
 package study.querydsl;
 
+import com.querydsl.core.QueryResults;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
@@ -12,6 +13,8 @@ import study.querydsl.entity.QMember;
 import study.querydsl.entity.Team;
 
 import javax.persistence.EntityManager;
+
+import java.util.List;
 
 import static study.querydsl.entity.QMember.member;
 
@@ -102,5 +105,50 @@ public class QuerydslBasicTest {
                 .fetchOne();
 
         Assertions.assertThat(findMember.getUsername()).isEqualTo("member1");
+    }
+
+    @Test
+    public void resultFetch() {
+
+        //리스트 조회
+        List<Member> fetch = queryFactory
+                .selectFrom(member)
+                .fetch();
+
+        //단건 조회
+        Member fetchOne = queryFactory
+                .selectFrom(member)
+                .fetchOne();
+
+        //처음 한 건 조회
+        Member fetchFirst = queryFactory
+                .selectFrom(member)
+                .fetchFirst();
+
+        //페이징에서 사용
+        QueryResults<Member> results = queryFactory
+                .selectFrom(member)
+                .fetchResults();
+
+//        results.getTotal();
+//        List<Member> contents = results.getResults();
+
+        long count = queryFactory
+                .selectFrom(member)
+                .fetchCount();
+
+    }
+
+    //위 deprecated 된 fetchResults 와 fetchCount 를 대체함.
+    //복잡한 쿼리에서는 위 두 메서드가 정상 작동하지 않기 때문에..
+    //fetch 와 아래 예시로 따로따로 관리 해주자.
+    @Test
+    public void count() {
+        Long totalCount = queryFactory
+                .select(member.count())
+                .from(member)
+                .fetchOne();
+
+        System.out.println("totalCount = " + totalCount);;
     }
 }
