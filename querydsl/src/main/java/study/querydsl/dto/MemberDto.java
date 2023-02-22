8ae30c1e9f214a6c0c17d44cd1e183dfb2917302
starryeye <see2818@naver.com>
2023-02-22 11:58:06 +0900

    [starryeye, 2023.02.22] - Querydsl, projection dto 조회
    내용
     - Querydsl, projection dto 조회
    개선

---
+++ querydsl/src/main/java/study/querydsl/dto/MemberDto.java
@@ -0,0 +1,21 @@
+package study.querydsl.dto;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * Member 엔티티 전체 말고 Member 엔티티의 속성 중..
+ * username, age 만 가지는 dto 클래스로 받기용
+ */
+@Data
+@NoArgsConstructor
+public class MemberDto {
+
+    private String username;
+    private int age;
+
+    public MemberDto(String username, int age) {
+        this.username = username;
+        this.age = age;
+    }
+}
