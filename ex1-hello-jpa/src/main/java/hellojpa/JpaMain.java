f489585dd1145daf9041e2f6e8a1ec05489afb94
starryeye <see2818@naver.com>
2023-03-16 00:11:26 +0900

    [starryeye, 2023.03.16] - 서블릿 컨테이너 초기화 인터페이스 구현
    내용
    - implements ServletContainerInitializer
    - override onStartup method

--- ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
+++ ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
@@ -21,43 +21,33 @@ public class JpaMain {
 
         try {
 
-            Member member = new Member();
-            member.setUsername("member1");
-            member.setHomeAddress(new Address("city1", "street1", "10000"));
+            Parent parent = new Parent();
+            parent.setName("p1");
 
-            member.getFavoriteFoods().add("치킨");
-            member.getFavoriteFoods().add("족발");
-            member.getFavoriteFoods().add("피자");
-
-            member.getAddressHistory().add(new AddressEntity("old1", "street", "10001"));
-            member.getAddressHistory().add(new AddressEntity("old2", "street", "10002"));
+            Child child1 = new Child();
+            child1.setName("c1");
 
 
-            entityManager.persist(member);
+            System.out.println("==========1===========");
+            entityManager.persist(parent);
+            System.out.println("==========2===========");
 
             entityManager.flush();
-            entityManager.clear();
+            entityManager.clear();;
 
-            System.out.println("================= Start ====================");
-            Member findMember = entityManager.find(Member.class, member.getId());
+            System.out.println("==========3===========");
+            Parent findParent = entityManager.find(Parent.class, parent.getId());
+            System.out.println("==========4===========");
 
-            //homeCity -> newCity
-//            Address a = findMember.getHomeAddress();
-//            findMember.setHomeAddress(new Address("newCity", a.getStreet(), a.getZipcode()));
+            findParent.addChild(child1);
 
-            //치킨을 한식으로
-//            findMember.getFavoriteFoods().remove("치킨");
-//            findMember.getFavoriteFoods().add("한식");
 
-            //이미 존재하는 old1을 지우고 싶을땐
-            //equals를 기본으로 동작한다..
-            //쿼리가 update로 되지 않고 delete, insert(2회, 컬랙션에 저장될 모든 요소) 각각 나간다.
-            //쓰지말자..
-            //findMember.getAddressHistory().remove(new Address("old1", "street", "10001"));
-            //findMember.getAddressHistory().add(new Address("newCity1", "street", "10001"));
 
+            System.out.println("==========5===========");
 
             tx.commit();
+
+            System.out.println("==========6===========");
         } catch (Exception e) {
             e.printStackTrace();
             tx.rollback();
