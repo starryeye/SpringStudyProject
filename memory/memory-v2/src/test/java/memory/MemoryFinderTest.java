fb7190b066550c216bd8944b4d28c09403e363d8
starryeye <see2818@naver.com>
2023-03-18 23:48:33 +0900

    [starryeye, 2023.03.18] - 순수 라이브러리를 개발 V2 init
    내용
    - memory 순수 라이브러리 개발 V2 init

---
+++ memory/memory-v2/src/test/java/memory/MemoryFinderTest.java
@@ -0,0 +1,17 @@
+package memory;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MemoryFinderTest {
+
+    @Test
+    void get() {
+        MemoryFinder memoryFinder = new MemoryFinder();
+        Memory memory = memoryFinder.get();
+        System.out.println("memory = " + memory);
+        Assertions.assertThat(memory).isNotNull();
+    }
+}
