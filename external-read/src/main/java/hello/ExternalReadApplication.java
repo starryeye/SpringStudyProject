0ca593b90a0980e5ad412795c63a4c68d4bd7acc
starryeye <see2818@naver.com>
2023-03-24 22:30:00 +0900

    [starryeye, 2023.03.24] - 외부 설정값을 편리하게 읽는 방법
    내용
    - 3. 객체 단위 묶음으로 읽기
    - 자바 빈 프로퍼티 방식 말고 생성자로 빈을 생성하여 데이터 변경 위험을 제거

--- external-read/src/main/java/hello/ExternalReadApplication.java
+++ external-read/src/main/java/hello/ExternalReadApplication.java
@@ -1,6 +1,7 @@
 package hello;
 
 import hello.config.MyDataSourceConfigV1;
+import hello.config.MyDataSourceConfigV2;
 import hello.config.MyDataSourceEnvConfig;
 import hello.config.MyDataSourceValueConfig;
 import org.springframework.boot.SpringApplication;
@@ -10,9 +11,10 @@ import org.springframework.context.annotation.Import;
 
 //@Import(MyDataSourceEnvConfig.class)
 //@Import(MyDataSourceValueConfig.class)
-@Import(MyDataSourceConfigV1.class)
+//@Import(MyDataSourceConfigV1.class)
+@Import(MyDataSourceConfigV2.class)
 @SpringBootApplication(scanBasePackages = "hello.datasource")
-@ConfigurationPropertiesScan
+//@ConfigurationPropertiesScan
 public class ExternalReadApplication {
 
     public static void main(String[] args) {
