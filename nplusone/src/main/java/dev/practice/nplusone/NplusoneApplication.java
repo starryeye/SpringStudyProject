36c2699427a7e6722ae4bc344867988f2b35e909
starryeye <see2818@naver.com>
2023-08-19 03:34:29 +0900

    [starryeye, 2023.08.19] - JPA N + 1 문제 project init
    내용
    - JPA N + 1 study

---
+++ nplusone/src/main/java/dev/practice/nplusone/NplusoneApplication.java
@@ -0,0 +1,13 @@
+package dev.practice.nplusone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NplusoneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NplusoneApplication.class, args);
+	}
+
+}
---
+++ nplusone/src/test/java/dev/practice/nplusone/NplusoneApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.practice.nplusone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NplusoneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
