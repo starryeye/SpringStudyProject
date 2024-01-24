73763fe2e4e0a568d8f92016d9f0e368bb7d4155
starryeye <see2818@naver.com>
2024-01-24 23:35:06 +0900

    [starryeye, 2024.01.24] - multiple datasource project init
    내용
    - multiple datasource 예제 만들어보기

---
+++ multipledatasources/src/test/java/dev/practice/multipledatasources/MultipledatasourcesApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.practice.multipledatasources;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MultipledatasourcesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
