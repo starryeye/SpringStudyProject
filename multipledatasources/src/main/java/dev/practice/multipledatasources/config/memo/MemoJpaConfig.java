42ba760a1b715649f2d01f07f3ef6426a5a9a2e1
starryeye <see2818@naver.com>
2024-01-27 02:32:05 +0900

    [starryeye, 2024.01.27] - multiple datasource config and develop
    내용
    - multiple datasource 예제 만들어보기

--- multipledatasources/src/main/java/dev/practice/multipledatasources/config/memo/MemoJpaConfig.java
+++ multipledatasources/src/main/java/dev/practice/multipledatasources/config/memo/MemoJpaConfig.java
@@ -1,7 +1,51 @@
 package dev.practice.multipledatasources.config.memo;
 
+import dev.practice.multipledatasources.repository.memo.MemoEntity;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.orm.jpa.EntityManagerFactoryBuilder;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import javax.sql.DataSource;
+import java.util.Objects;
 
 @Configuration
+@EnableTransactionManagement
+@EnableJpaRepositories( // JPA repository 빈을 등록하도록 하는 어노테이션
+        basePackages = "dev.practice.multipledatasources.repository.memo", // 스캔범위
+        entityManagerFactoryRef = "memosEntityManagerFactory",
+        transactionManagerRef = "memosTransactionManager"
+)
 public class MemoJpaConfig {
+
+    @Bean
+    public LocalContainerEntityManagerFactoryBean memosEntityManagerFactory(
+            @Qualifier("memosDataSource") DataSource dataSource,
+            EntityManagerFactoryBuilder entityManagerFactoryBuilder
+    ) {
+
+        Class<?>[] targetEntities = {MemoEntity.class};
+
+        // EntityManagerFactory 생성
+        return entityManagerFactoryBuilder
+                .dataSource(dataSource)
+                .packages(targetEntities)
+                .build();
+    }
+
+    @Bean
+    public PlatformTransactionManager memosTransactionManager(
+            @Qualifier("memosEntityManagerFactory") LocalContainerEntityManagerFactoryBean entityManagerFactory
+    ) {
+
+        // TransactionManager 생성
+        return new JpaTransactionManager(
+                Objects.requireNonNull(entityManagerFactory.getObject())
+        );
+    }
 }
