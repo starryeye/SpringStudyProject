10212befa2ac62fe8244a09a6ea4465499474325
starryeye <see2818@naver.com>
2024-02-03 19:29:11 +0900

    [starryeye, 2024.02.03] - JPA 복합 키 구현 및 test
    내용
    - embeddable
    - IdClass

--- compositekey/src/main/java/dev/practice/compositekey/idclass/School.java
+++ compositekey/src/main/java/dev/practice/compositekey/idclass/School.java
@@ -1,7 +1,34 @@
 package dev.practice.compositekey.idclass;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.IdClass;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Entity
+@IdClass(SchoolId.class)
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class School {
 
+    @Id
     private String regionNumber;
+    @Id
     private String name;
+
+    @Builder
+    private School(String regionNumber, String name) {
+        this.regionNumber = regionNumber;
+        this.name = name;
+    }
+
+    public static School create(String regionNumber, String name) {
+        return School.builder()
+                .regionNumber(regionNumber)
+                .name(name)
+                .build();
+    }
 }
--- compositekey/src/main/java/dev/practice/compositekey/idclass/SchoolId.java
+++ compositekey/src/main/java/dev/practice/compositekey/idclass/SchoolId.java
@@ -1,4 +1,21 @@
 package dev.practice.compositekey.idclass;
 
-public class SchoolId {
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+
+@NoArgsConstructor
+public class SchoolId implements Serializable {
+
+    private String regionNumber;
+    private String name;
+
+    /**
+     * 복합키 식별자 클래스
+     *
+     * 식별자 클래스의 속성명과 엔티티에서 사용하는 식별자의 속성명이 같아야한다.
+     * Serializable 을 구현한다.
+     * 기본 생성자가 존재해야한다.
+     * 식별자는 public class 여야한다.
+     */
 }
