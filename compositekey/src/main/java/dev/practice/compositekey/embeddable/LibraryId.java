d281b7150a03c9ba6300c0267331b7fe69fe4728
starryeye <see2818@naver.com>
2024-02-03 19:21:32 +0900

    [starryeye, 2024.02.03] - JPA 복합 키 구현 및 test
    내용
    - embeddable
    - IdClass

--- compositekey/src/main/java/dev/practice/compositekey/embeddable/LibraryId.java
+++ compositekey/src/main/java/dev/practice/compositekey/embeddable/LibraryId.java
@@ -3,10 +3,12 @@ package dev.practice.compositekey.embeddable;
 import jakarta.persistence.Embeddable;
 import lombok.Builder;
 import lombok.EqualsAndHashCode;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import java.io.Serializable;
 
+@Getter
 @NoArgsConstructor
 @EqualsAndHashCode
 @Embeddable
---
+++ compositekey/src/test/java/dev/practice/compositekey/embeddable/LibraryRepositoryTest.java
@@ -0,0 +1,46 @@
+package dev.practice.compositekey.embeddable;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.time.LocalDate;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@Slf4j
+@SpringBootTest
+class LibraryRepositoryTest {
+
+
+    @Autowired
+    private LibraryRepository libraryRepository;
+
+    @DisplayName("복합키로 구성된 entity 를 다루어본다.")
+    @Test
+    void insert_and_select() {
+
+        // given
+        Library library = Library.create("1", "national central library", LocalDate.now());
+
+        // when
+        libraryRepository.save(library);
+
+        // then
+        Library result = libraryRepository.findById(library.getId()).orElseThrow();
+
+        assertThat(result.getId().getRegionNumber())
+                .isEqualTo(library.getId().getRegionNumber());
+        assertThat(result.getId().getName())
+                .isEqualTo(library.getId().getName());
+        assertThat(result.getEstablishmentDate())
+                .isEqualTo(library.getEstablishmentDate());
+
+        // library 와 result 는 서로 다른 영속성 컨텍스트에서 관리된 엔티티 이므로 인스턴스 다름
+        // library 의 복합키 인스턴스와 result 복합키 인스턴스가 같은 이유는 (동일성) String 으로 이루어져 있어서 동등한 값이면 동일해서 인듯
+        log.info("library = {}, library.getId() = {}", library, library.getId()); // getId() 를 출력하면 LibraryId 의 toString 출력 -> hashcode() 출력
+        log.info("result = {}, result.getId() = {}", result, result.getId());
+    }
+}
