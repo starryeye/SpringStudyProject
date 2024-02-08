edd68ce02861ac0a0b809ab80f61c62562f440c3
starryeye <see2818@naver.com>
2024-02-08 19:24:06 +0900

    [starryeye, 2024.02.08] - eager and lazy loading example project init
    내용
    - eager and lazy loading example project init

---
+++ eagerandlazy/src/main/java/dev/practice/eagerandlazy/EagerandlazyApplication.java
@@ -0,0 +1,13 @@
+package dev.practice.eagerandlazy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EagerandlazyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EagerandlazyApplication.class, args);
+	}
+
+}
---
+++ eagerandlazy/src/test/java/dev/practice/eagerandlazy/EagerandlazyApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.practice.eagerandlazy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EagerandlazyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
