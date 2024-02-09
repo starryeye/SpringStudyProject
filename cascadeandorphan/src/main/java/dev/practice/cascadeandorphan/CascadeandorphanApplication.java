dba078d58d184a997a97307feb3ee1ae8a74f479
starryeye <see2818@naver.com>
2024-02-09 19:16:44 +0900

    [starryeye, 2024.02.09] - cascade and orphan example project init
    내용
    - cascade and orphan example project init

---
+++ cascadeandorphan/src/main/java/dev/practice/cascadeandorphan/CascadeandorphanApplication.java
@@ -0,0 +1,13 @@
+package dev.practice.cascadeandorphan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CascadeandorphanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CascadeandorphanApplication.class, args);
+	}
+
+}
---
+++ cascadeandorphan/src/test/java/dev/practice/cascadeandorphan/CascadeandorphanApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.practice.cascadeandorphan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CascadeandorphanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
