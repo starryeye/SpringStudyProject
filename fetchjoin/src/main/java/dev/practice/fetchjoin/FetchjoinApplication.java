2ccfaa513babfe41e1c9ef9d67a3564892663d49
starryeye <see2818@naver.com>
2024-02-10 22:07:47 +0900

    [starryeye, 2024.02.10] - 페치 조인 example project init
    내용
    - 페치 조인 example project init

---
+++ fetchjoin/src/main/java/dev/practice/fetchjoin/FetchjoinApplication.java
@@ -0,0 +1,13 @@
+package dev.practice.fetchjoin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FetchjoinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FetchjoinApplication.class, args);
+	}
+
+}
