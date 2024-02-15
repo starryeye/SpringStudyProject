3c1435da374d98ed8dd41f7c3b613c89415c5de9
starryeye <see2818@naver.com>
2024-02-15 21:38:30 +0900

    [starryeye, 2024.02.15] - Spring, JPA, osiv example project init
    내용
    - Spring, JPA, osiv example project init

---
+++ osiv/src/main/java/dev/practice/osiv/OsivApplication.java
@@ -0,0 +1,13 @@
+package dev.practice.osiv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OsivApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OsivApplication.class, args);
+	}
+
+}
---
+++ osiv/src/test/java/dev/practice/osiv/OsivApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.practice.osiv;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OsivApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
