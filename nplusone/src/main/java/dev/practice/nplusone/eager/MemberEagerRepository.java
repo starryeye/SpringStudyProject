374cd3364af0aa5b518e8631538ad5edae6c5c98
starryeye <see2818@naver.com>
2024-02-19 22:26:59 +0900

    [starryeye, 2024.02.19] - N + 1 problem example code
    내용
    - N + 1 problem example code

--- nplusone/src/main/java/dev/practice/nplusone/eager/MemberEagerRepository.java
+++ nplusone/src/main/java/dev/practice/nplusone/eager/MemberEagerRepository.java
@@ -12,7 +12,7 @@ public interface MemberEagerRepository extends JpaRepository<MemberEager, Long> 
     @Query(value = "select m from MemberEager m where m.id = :id")
     Optional<MemberEager> findMemberEagerBy(Long id);
 
-    @EntityGraph(attributePaths = {"teamEager"})
+    @EntityGraph(attributePaths = {"teamEager"}) // 참고로 메서드 이름 조회 뿐만 아니라 JPQL 에도 적용할 수 있다.
     List<MemberEager> findEntityGraphByIdIn(List<Long> ids);
 
     List<MemberEager> findByIdIn(List<Long> ids);
