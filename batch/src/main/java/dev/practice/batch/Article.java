f346d5dadfc417839fa0f2dedca40c2ac6380c48
starryeye <see2818@naver.com>
2024-02-22 20:52:58 +0900

    [starryeye, 2024.02.22] - JPA batch example project init
    내용
    - JPA batch example project init

--- batch/src/main/java/dev/practice/batch/Article.java
+++ batch/src/main/java/dev/practice/batch/Article.java
@@ -15,7 +15,7 @@ import lombok.NoArgsConstructor;
 public class Article {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
     private Long id;
 
     private String title;
