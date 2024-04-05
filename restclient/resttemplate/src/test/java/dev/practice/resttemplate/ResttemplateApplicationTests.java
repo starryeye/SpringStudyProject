a9fe93159a9bee0a1d3593a624e369688dcbc749
starryeye <see2818@naver.com>
2024-04-05 22:23:33 +0900

    [starryeye, 2024.04.05] - resttemplate init
    내용
    - resttemplate init

---
+++ restclient/resttemplate/src/test/java/dev/practice/resttemplate/ResttemplateApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.practice.resttemplate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResttemplateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
