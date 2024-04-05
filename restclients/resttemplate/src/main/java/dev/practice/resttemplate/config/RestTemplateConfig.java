ba735a2eee43c351a0f9641aa22b832d5071f917
starryeye <see2818@naver.com>
2024-04-05 23:48:17 +0900

    [starryeye, 2024.04.05] - resttemplate timeout
    내용
    - resttemplate timeout

--- restclients/resttemplate/src/main/java/dev/practice/resttemplate/config/RestTemplateConfig.java
+++ restclients/resttemplate/src/main/java/dev/practice/resttemplate/config/RestTemplateConfig.java
@@ -1,2 +1,25 @@
-package dev.practice.resttemplate.config;public class RestTemplateConfig {
+package dev.practice.resttemplate.config;
+
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
+
+import java.time.Duration;
+
+@Configuration
+public class RestTemplateConfig {
+
+    /**
+     * Spring 은 기본적으로 AutoConfiguration 에 의해
+     * RestTemplateBuilder 를 빈으로 등록해두므로 RestTemplate 빈 등록시 이용하면 된다.
+     */
+
+    @Bean // with timeout
+    public RestTemplate restTemplateWithTimeout(RestTemplateBuilder restTemplateBuilder) {
+        return restTemplateBuilder
+                .setConnectTimeout(Duration.ofSeconds(5))
+                .setReadTimeout(Duration.ofSeconds(5))
+                .build();
+    }
 }
