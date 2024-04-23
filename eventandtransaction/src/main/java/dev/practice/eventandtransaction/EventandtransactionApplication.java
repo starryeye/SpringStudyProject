62df54a2aa2de253e01f5a02467cc674bc7cba49
starryeye <see2818@naver.com>
2024-04-24 01:43:14 +0900

    [starryeye, 2024.04.24] - spring event and transaction init
    내용
    - spring event and transaction init

---
+++ eventandtransaction/src/main/java/dev/practice/eventandtransaction/EventandtransactionApplication.java
@@ -0,0 +1,13 @@
+package dev.practice.eventandtransaction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventandtransactionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventandtransactionApplication.class, args);
+	}
+
+}
---
+++ eventandtransaction/src/test/java/dev/practice/eventandtransaction/EventandtransactionApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.practice.eventandtransaction;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventandtransactionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
