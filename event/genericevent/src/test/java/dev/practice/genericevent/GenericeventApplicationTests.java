e3c616a38eff14c8f84de74a763725551eabbb75
starryeye <see2818@naver.com>
2024-04-25 05:38:43 +0900

    [starryeye, 2024.04.25] - spring event (spring 4.2 이후 방식) init
    내용
    - init

---
+++ event/genericevent/src/test/java/dev/practice/genericevent/GenericeventApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.practice.genericevent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GenericeventApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
