651f1f3c49db88f1aaed3a4f82bbb13a902b257d
starryeye <see2818@naver.com>
2024-04-25 20:22:30 +0900

    [starryeye, 2024.04.25] - spring event (spring 4.2 이전 방식)
    내용
    - Sync : ApplicationEventPublisher, ApplicationListener, ApplicationEvent
    - Async : ApplicationEventMulticaster, TaskExecutor

--- event/applicationevent/src/main/java/dev/practice/applicationevent/async/AsynchronousSpringEventsConfig.java
+++ event/applicationevent/src/main/java/dev/practice/applicationevent/async/AsynchronousSpringEventsConfig.java
@@ -1,8 +1,14 @@
 package dev.practice.applicationevent.async;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.context.event.ApplicationEventMulticaster;
 import org.springframework.context.event.SimpleApplicationEventMulticaster;
 import org.springframework.core.task.SimpleAsyncTaskExecutor;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+
+import java.util.concurrent.Executor;
+import static org.springframework.context.support.AbstractApplicationContext.APPLICATION_EVENT_MULTICASTER_BEAN_NAME;
 
 //@Configuration
 public class AsynchronousSpringEventsConfig {
@@ -12,12 +18,24 @@ public class AsynchronousSpringEventsConfig {
      * Test 에서만 켜주도록 한다. -> async.config.AsyncTestConfig
      */
 
-//    @Bean
-    public ApplicationEventMulticaster applicationEventMulticaster() { // bean 이름은 applicationEventMulticaster 이어야 한다.
+//    @Bean(name = APPLICATION_EVENT_MULTICASTER_BEAN_NAME)
+    public ApplicationEventMulticaster applicationEventMulticaster() { // bean 이름은 applicationEventMulticaster 이어야 한다. 혹은 여기서와 같이 APPLICATION_EVENT_MULTICASTER_BEAN_NAME 를 사용해준다.
 
         SimpleApplicationEventMulticaster eventMulticaster = new SimpleApplicationEventMulticaster();
 
-        eventMulticaster.setTaskExecutor(new SimpleAsyncTaskExecutor()); // 모든 Spring Event 리스너는 SimpleAsyncTaskExecutor 스레드로 이벤트를 비동기로 처리한다.
+//        eventMulticaster.setTaskExecutor(new SimpleAsyncTaskExecutor()); // 모든 Spring Event 리스너는 SimpleAsyncTaskExecutor 스레드로 이벤트를 비동기로 처리한다.
+        eventMulticaster.setTaskExecutor(asyncExecutor()); // 모든 Spring Event 리스너는 asyncExecutor() 반환 스레드로 이벤트를 비동기로 처리한다.
         return eventMulticaster;
+    }
+
+    private Executor asyncExecutor() {
+        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+        executor.setCorePoolSize(10);
+        executor.setMaxPoolSize(10);
+        executor.setQueueCapacity(10000);
+        executor.setWaitForTasksToCompleteOnShutdown(true);
+        executor.setAwaitTerminationSeconds(10);
+        executor.initialize();
+        return executor;
     }
 }
