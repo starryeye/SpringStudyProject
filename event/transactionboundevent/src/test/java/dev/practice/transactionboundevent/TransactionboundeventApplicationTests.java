daffd08d7ac543ae3aa68552194112263cdf613d
starryeye <see2818@naver.com>
2024-04-25 21:49:52 +0900

    [starryeye, 2024.04.25] - spring transaction bound event init
    내용
    - spring transaction bound event init

---
+++ event/transactionboundevent/src/test/java/dev/practice/transactionboundevent/TransactionboundeventApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.practice.transactionboundevent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransactionboundeventApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
