481ddd5d87b3274db6027ce9d8992253960ea094
starryeye <see2818@naver.com>
2024-04-27 00:19:18 +0900

    [starryeye, 2024.04.27] - tight coupling -> loose coupling init
    내용
    - tight coupling -> loose coupling init

---
+++ event/coupling/src/main/java/dev/practice/coupling/CouplingApplication.java
@@ -0,0 +1,13 @@
+package dev.practice.coupling;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CouplingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CouplingApplication.class, args);
+	}
+
+}
---
+++ event/coupling/src/test/java/dev/practice/coupling/CouplingApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.practice.coupling;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CouplingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
