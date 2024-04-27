33d812b71c632a5bb928733169c50f7007244c7b
starryeye <see2818@naver.com>
2024-04-27 22:51:25 +0900

    [starryeye, 2024.04.27] - phase 3, async 처리
    내용
    - tight coupling, 문제점

--- event/coupling/src/main/java/dev/practice/coupling/CouplingApplication.java
+++ event/coupling/src/main/java/dev/practice/coupling/CouplingApplication.java
@@ -6,6 +6,28 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class CouplingApplication {
 
+	/**
+	 * 예제 비즈니스 - 회원 가입
+	 * -> 1. DB 에 회원 정보 저장 (main)
+	 * -> 2. 회원 가입 감사 이메일 보내기, DB 에 회원 가입 감사 이메일 히스토리 저장 (sub)
+	 *
+	 *
+	 * 목적
+	 * 회원 가입 비즈니스를 가지고 하나의 모듈 내에서
+	 * tight coupling 된 상황을 알아보고 loose coupling 해본다.
+	 *
+	 *
+	 *
+	 * 각 단계에 대한 부가 설명은 아래를 참조
+	 * phase1 -> MemberServiceV1
+	 * phase2 -> RegisterMemberFacadeV2
+	 * phase3 -> MemberServiceV3
+	 * phase4 ->
+	 *
+	 * 참고해보면 좋음
+	 * https://youtu.be/b65zIH7sDug?si=rvxZui-mR8-a3k_S
+	 */
+
 	public static void main(String[] args) {
 		SpringApplication.run(CouplingApplication.class, args);
 	}
--- event/coupling/src/main/java/dev/practice/coupling/phase3_async/service/MemberServiceV3.java
+++ event/coupling/src/main/java/dev/practice/coupling/phase3_async/service/MemberServiceV3.java
@@ -33,6 +33,35 @@ public class MemberServiceV3 {
     }
 
     /**
-     * 
+     * phase 3 에서는 phase 1 과 비교하여 EmailService 에서 @Async 를 활용하여
+     * EmailService 를 비동기 처리하였다.
+     *
+     * 이로써, phase 1 문제 중.. 어떤게 해결되었는지 보자..
+     *
+     * 1. 기능간 강결합
+     * 해결되지 않음
+     * phase 1 이유와 동일
+     *
+     * 2. transaction 경계
+     * 해결됨
+     * 트랜잭션은 ThreadLocal 기반으로 동일 Connection 내에서 동작한다.
+     * @Async 로 서로 다른 스레드로 동작하도록 만들었기 때문에 트랜잭션이 분리되었다.
+     * 메인 로직인 회원 가입은 성공하고 메일 보내기 기능이 실패해도 메인 로직이 롤백되지는 않는다.
+     *
+     * 3. 동기적인 방식
+     * 해결됨
+     * MemberService 로직에서 EmailService 를 호출하지만, @Async 로 인해 서로 다른 스레드로 동작하며 (non-blocking)
+     * MemberService 은 EmailService 완료 시점 및 결과에 아무런 관심이 없어졌으므로 비동기이다.
+     * -> EmailService 처리 시간은 API 응답 시간에 영향을 주지 않게 되었다.
+     *
+     * 4. commit 시점과 메일 발송 시점
+     * 해결되지 않음
+     * 스레드가 분리 되어도..
+     * 현재 코드 구조상 phase 2 의 두번째 방법(@Transactional 에 REQUIRED_NEW 전파 옵션을 사용) 과 마찬가지로
+     * MemberService 내부에서 EmailService 를 호출 하기 때문에
+     * MemberService 트랜잭션 commit 은 EmailService 로직을 호출한 이후에 수행된다.
+     * 결국, phase 1 의 참고에서 말한 문제가 생길 수 있음
+     * -> 근데, facade 계층을 두면 해결해볼 순 있음..
+     *
      */
 }
