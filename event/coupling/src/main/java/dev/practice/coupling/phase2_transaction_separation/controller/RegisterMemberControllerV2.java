36f1a1107e18183ec3039cf7b7931d6c453a9061
starryeye <see2818@naver.com>
2024-04-27 21:25:08 +0900

    [starryeye, 2024.04.27] - phase 2, transaction 분리
    내용
    - tight coupling, 문제점

--- event/coupling/src/main/java/dev/practice/coupling/phase2_transaction_separation/controller/RegisterMemberControllerV2.java
+++ event/coupling/src/main/java/dev/practice/coupling/phase2_transaction_separation/controller/RegisterMemberControllerV2.java
@@ -1,7 +1,12 @@
 package dev.practice.coupling.phase2_transaction_separation.controller;
 
+import dev.practice.coupling.dto.RegisterMemberRequest;
+import dev.practice.coupling.dto.RegisterMemberResponse;
 import dev.practice.coupling.phase2_transaction_separation.RegisterMemberFacadeV2;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -9,4 +14,10 @@ import org.springframework.web.bind.annotation.RestController;
 public class RegisterMemberControllerV2 {
 
     private final RegisterMemberFacadeV2 registerMemberFacadeV2;
+
+    @PostMapping("/api/v1/phase2/new")
+    public ResponseEntity<RegisterMemberResponse> registerMember(@RequestBody RegisterMemberRequest request) {
+
+        return ResponseEntity.ok(registerMemberFacadeV2.execute(request));
+    }
 }
