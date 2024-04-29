3db2476ccf74577f59d848cda95366d745010d64
starryeye <see2818@naver.com>
2024-04-29 21:22:09 +0900

    [starryeye, 2024.04.29] - phase 1, test code
    내용
    - phase 1, test code

--- event/coupling/src/main/java/dev/practice/coupling/repository/EmailSendHistoryRepository.java
+++ event/coupling/src/main/java/dev/practice/coupling/repository/EmailSendHistoryRepository.java
@@ -2,5 +2,10 @@ package dev.practice.coupling.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface EmailSendHistoryRepository extends JpaRepository<EmailSendHistory, Long> {
+
+    // for test
+    List<EmailSendHistory> findByTargetMemberId(Long memberId);
 }
