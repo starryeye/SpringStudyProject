f6123b3ce5a1f6fc9d2cf644f377fcf868e573a4
starryeye <see2818@naver.com>
2024-04-29 21:48:51 +0900

    [starryeye, 2024.04.29] - phase 4, test code
    내용
    - phase 4, test code

--- event/coupling/src/main/java/dev/practice/coupling/phase4_event/service/EmailServiceV4.java
+++ event/coupling/src/main/java/dev/practice/coupling/phase4_event/service/EmailServiceV4.java
@@ -17,7 +17,7 @@ public class EmailServiceV4 {
     private final EmailSender emailSender;
     private final EmailSendHistoryRepository emailSendHistoryRepository;
 
-    private static final String THANK_YOU = "%s, Thank you for using our service!";
+    private static final String THANK_YOU = "%s, Thank you for using our service! (phase 4)";
 
     @Transactional
     public void sendThankYouEmailForRegistered(Long memberId, String memberName, String emailAddress) {
