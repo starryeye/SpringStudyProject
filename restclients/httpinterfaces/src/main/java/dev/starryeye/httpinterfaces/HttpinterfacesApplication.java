e8b78b21c66db351e9f9d524a507303476d936e5
starryeye <see2818@naver.com>
2024-05-27 21:47:19 +0900

    [starryeye, 2024.05.27] - Http Interfaces 사용법
    내용
    - solved

---
+++ restclients/httpinterfaces/src/main/java/dev/starryeye/httpinterfaces/HttpinterfacesApplication.java
@@ -0,0 +1,13 @@
+package dev.starryeye.httpinterfaces;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HttpinterfacesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HttpinterfacesApplication.class, args);
+	}
+
+}
---
+++ restclients/httpinterfaces/src/test/java/dev/starryeye/httpinterfaces/HttpinterfacesApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.starryeye.httpinterfaces;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HttpinterfacesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
