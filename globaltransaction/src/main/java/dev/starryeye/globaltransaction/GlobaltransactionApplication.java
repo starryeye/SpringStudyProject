de03da9f1bff49b00f091c154785b4efbbc18980
starryeye <see2818@naver.com>
2024-08-17 19:20:09 +0900

    [starryeye, 2024.08.17] - Global Transaction, JTA init
    내용
    - Global Transaction, JTA init

---
+++ globaltransaction/src/main/java/dev/starryeye/globaltransaction/GlobaltransactionApplication.java
@@ -0,0 +1,13 @@
+package dev.starryeye.globaltransaction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GlobaltransactionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GlobaltransactionApplication.class, args);
+	}
+
+}
